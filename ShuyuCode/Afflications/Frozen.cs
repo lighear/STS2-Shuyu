b4689ec138fd8e26e4454adfd0b72c92c99fdc39
lighear <1352615088@qq.com>
2026-06-08 12:57:32 +0800

    初始卡（3/3）

---
+++ ShuyuCode/Afflications/Frozen.cs
@@ -0,0 +1,33 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using Shuyu.Cards;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuyu.Afflications
+{
+    [RegisterAffliction]
+    public class Frozen : ModAfflictionTemplate
+    {
+        public override void AfterApplied()
+        {
+            base.AfterApplied();
+            ShuyuMechanismHelper.FreezeCard(base.Card);
+        }
+
+        public override void BeforeRemoved()
+        {
+            base.BeforeRemoved();
+            if (base.Card is FrozenCardModel card)
+            {
+                ShuyuMechanismHelper.UnfreezeCard(card);
+            }
+        }
+    }
+}
