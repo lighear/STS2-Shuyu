6c8ec6f8d6705e71028bf3bcbc095fb811f7aa1f
mizore <335799697@qq.com>
2026-07-12 21:11:05 +0800

    尝试解决置换术式的易碎转换奖励会被提前伤害吞掉的问题。

--- ShuyuCode/Powers/FragilePower.cs
+++ ShuyuCode/Powers/FragilePower.cs
@@ -36,6 +36,28 @@ public class FragilePower : ModPowerTemplate
         new DynamicVar("DamageIncrease", 1.25m)
     ];
 
+    private class Data
+    {
+        public bool isConverting;
+    }
+
+    private bool IsConverting
+    {
+        get
+        {
+            return GetInternalData<Data>().isConverting;
+        }
+        set
+        {
+            GetInternalData<Data>().isConverting = value;
+        }
+    }
+
+    protected override object? InitInternalData()
+    {
+        return new Data() { isConverting = false };
+    }
+
     public override decimal ModifyDamageMultiplicative(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource, MegaCrit.Sts2.Core.Entities.Cards.CardPlay? cardPlay)
     {
         if (target != Owner || !props.IsPoweredAttack() || target.GetPower<VulnerablePower>() != null)
@@ -47,21 +69,41 @@ public class FragilePower : ModPowerTemplate
 
     public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
     {
-        if (power == this && Amount >= 5)
+        if (power == this)
+        {
+            await ConvertIfThresholdMet(choiceContext, applier);
+        }
+    }
+
+    public async Task ConvertIfThresholdMet(PlayerChoiceContext choiceContext, Creature? applier)
+    {
+        if (Amount < 5 || IsConverting)
+        {
+            return;
+        }
+
+        IsConverting = true;
+        try
         {
             Flash();
-            await PowerCmd.Apply<VulnerablePower>(choiceContext, Owner, 3, applier, null);
+            IOnFragileConverted[] listeners =
+                CombatState?.IterateHookListeners().OfType<IOnFragileConverted>().ToArray() ?? [];
 
             await PowerCmd.ModifyAmount(choiceContext, this, -5, null, null);
 
-            if (CombatState == null)
-            {
-                return;
-            }
-            foreach (IOnFragileConverted ip in CombatState.IterateHookListeners().OfType<IOnFragileConverted>())
+            foreach (IOnFragileConverted ip in listeners.OrderBy(ip => ip is ZhiHuanShuShiPower ? 0 : 1))
             {
                 await ip.OnFragileConverted(choiceContext, Owner, applier);
             }
+
+            if (Owner.CombatState != null && Owner.IsAlive)
+            {
+                await PowerCmd.Apply<VulnerablePower>(choiceContext, Owner, 3, applier, null);
+            }
+        }
+        finally
+        {
+            IsConverting = false;
         }
     }
 }
