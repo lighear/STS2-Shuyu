5337696c6a076b4bf1acede78c05178c0dcd5451
mizore <335799697@qq.com>
2026-07-17 01:13:37 +0800

    ai制作寒冰荆棘的特效

--- ShuyuCode/Vfx/VfxUtil.cs
+++ ShuyuCode/Vfx/VfxUtil.cs
@@ -23,6 +23,7 @@ public static class VFXUtil
             "res://Shuyu/scenes/vfx_ChillPower_background.tscn",
             "res://Shuyu/scenes/vfx_BingWuPower.tscn",
             "res://Shuyu/scenes/vfx_WanBiBuPoPower_ring.tscn",
+            "res://Shuyu/scenes/vfx_IceThornsPower.tscn",
             "res://Shuyu/scenes/vfx/vfx_HanXingZhuiLuo.tscn",
         };
         foreach (var path in paths)
