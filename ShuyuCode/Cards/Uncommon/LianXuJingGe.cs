0c510d1c484df7c78a30e2bbcfdd4aad9f2225be
mizore <335799697@qq.com>
2026-07-18 13:29:55 +0800

    平衡性调整：略微减少防护凝结+、冰晶之舞、连续晶格的格挡数值。

--- ShuyuCode/Cards/Uncommon/LianXuJingGe.cs
+++ ShuyuCode/Cards/Uncommon/LianXuJingGe.cs
@@ -35,7 +35,7 @@ namespace Shuyu.Cards
         ];
 
         protected override IEnumerable<DynamicVar> CanonicalVars => [
-            new BlockVar(4, ValueProp.Unpowered),
+            new BlockVar(3, ValueProp.Unpowered),
             new CardsVar(1),
             new EnergyVar(1)
         ];
